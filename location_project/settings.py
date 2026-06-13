115f17d0d82caf879720924adc698a6b0aaec062
Anoop Sharma <anoopsharma@192.168.1.4>
2026-06-13 12:05:35 +0530

    Setup deployment

--- location_project/settings.py
+++ location_project/settings.py
@@ -82,7 +82,8 @@ WSGI_APPLICATION = 'location_project.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
     )
 }
 
